e6eeb0ce125b1fbf6dd5cca127c4eb3124862dbf
RagilPratama <ragilpratamaaa@gmail.com>
2023-01-26 10:56:01 +0700

    Base Dashboard

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,11 @@ module.exports = {
   ],
   plugins: ['react', 'react-hooks'],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-  },
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": 
+    [ "error",
+      {
+        "endOfLine": "auto"}
+    ]
+  }
 }
